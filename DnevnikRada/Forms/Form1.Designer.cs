83e4663fcbbb6a5456ba2d32481fb3a57f0eddde
sandiivanusec <s.ivanusec@gmail.com>
2018-12-25 16:26:06 +0100

    promjenjen UI na form1

--- DnevnikRada/Forms/Form1.Designer.cs
+++ DnevnikRada/Forms/Form1.Designer.cs
@@ -131,9 +131,9 @@
             // 
             // lbl_evidencija
             // 
-            this.lbl_evidencija.BackColor = System.Drawing.Color.Black;
-            this.lbl_evidencija.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.lbl_evidencija.ForeColor = System.Drawing.Color.Aqua;
+            this.lbl_evidencija.BackColor = System.Drawing.Color.AliceBlue;
+            this.lbl_evidencija.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lbl_evidencija.ForeColor = System.Drawing.Color.Black;
             this.lbl_evidencija.Location = new System.Drawing.Point(-1, 0);
             this.lbl_evidencija.Name = "lbl_evidencija";
             this.lbl_evidencija.Size = new System.Drawing.Size(300, 40);
@@ -143,9 +143,9 @@
             // 
             // lbl_skladiste
             // 
-            this.lbl_skladiste.BackColor = System.Drawing.Color.Black;
-            this.lbl_skladiste.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.lbl_skladiste.ForeColor = System.Drawing.Color.Aqua;
+            this.lbl_skladiste.BackColor = System.Drawing.Color.AliceBlue;
+            this.lbl_skladiste.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lbl_skladiste.ForeColor = System.Drawing.Color.Black;
             this.lbl_skladiste.Location = new System.Drawing.Point(-1, 290);
             this.lbl_skladiste.Name = "lbl_skladiste";
             this.lbl_skladiste.Size = new System.Drawing.Size(300, 40);
@@ -155,9 +155,9 @@
             // 
             // lbl_stanje_poduzeca
             // 
-            this.lbl_stanje_poduzeca.BackColor = System.Drawing.Color.Black;
-            this.lbl_stanje_poduzeca.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.lbl_stanje_poduzeca.ForeColor = System.Drawing.Color.Aqua;
+            this.lbl_stanje_poduzeca.BackColor = System.Drawing.Color.AliceBlue;
+            this.lbl_stanje_poduzeca.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lbl_stanje_poduzeca.ForeColor = System.Drawing.Color.Black;
             this.lbl_stanje_poduzeca.Location = new System.Drawing.Point(300, 0);
             this.lbl_stanje_poduzeca.Name = "lbl_stanje_poduzeca";
             this.lbl_stanje_poduzeca.Size = new System.Drawing.Size(300, 40);
@@ -167,9 +167,9 @@
             // 
             // lbl_mjesta
             // 
-            this.lbl_mjesta.BackColor = System.Drawing.Color.Black;
-            this.lbl_mjesta.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
-            this.lbl_mjesta.ForeColor = System.Drawing.Color.Aqua;
+            this.lbl_mjesta.BackColor = System.Drawing.Color.AliceBlue;
+            this.lbl_mjesta.Font = new System.Drawing.Font("Century Gothic", 15.75F, System.Drawing.FontStyle.Underline, System.Drawing.GraphicsUnit.Point, ((byte)(238)));
+            this.lbl_mjesta.ForeColor = System.Drawing.Color.Black;
             this.lbl_mjesta.Location = new System.Drawing.Point(300, 290);
             this.lbl_mjesta.Name = "lbl_mjesta";
             this.lbl_mjesta.Size = new System.Drawing.Size(300, 40);
@@ -179,6 +179,8 @@
             // 
             // panel_zadaci_aktivnog_tjedna_i_obavijesti
             // 
+            this.panel_zadaci_aktivnog_tjedna_i_obavijesti.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("panel_zadaci_aktivnog_tjedna_i_obavijesti.BackgroundImage")));
+            this.panel_zadaci_aktivnog_tjedna_i_obavijesti.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Stretch;
             this.panel_zadaci_aktivnog_tjedna_i_obavijesti.Controls.Add(this.kalendar);
             this.panel_zadaci_aktivnog_tjedna_i_obavijesti.HorizontalScrollbarBarColor = true;
             this.panel_zadaci_aktivnog_tjedna_i_obavijesti.HorizontalScrollbarHighlightOnWheel = false;
