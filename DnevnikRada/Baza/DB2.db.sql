7a4ce19dfe441592d4d46dff22acb081ab65b65c
Danko <danko.salopek1@gmail.com>
2019-01-22 00:23:56 +0100

    update db2

--- DnevnikRada/Baza/DB2.db.sql
+++ DnevnikRada/Baza/DB2.db.sql
@@ -13,19 +13,23 @@ INSERT INTO `Mjesta` (NazivMjesta,Adresa) VALUES ('Rijeka','Nojev2 36');
 INSERT INTO `Mjesta` (NazivMjesta,Adresa) VALUES ('Pula','Kebab 77');
 INSERT INTO `Evidencija` (NazivMjesta, Datum, OpisPOsla, UtrosenoVrijeme, ID_Mjesta) VALUES ('Rijeka','2018-01-01 11:09:08','Samo za dodavanje svih materijala',10,2);
 INSERT INTO `Evidencija` (NazivMjesta, Datum, OpisPOsla, UtrosenoVrijeme, ID_Mjesta) VALUES ('Rijeka','2019-01-10 11:09:08','Prodano kupljeno nabavljeno nije bitno nesto sam radio',10,2);
-INSERT INTO `Evidencija` (NazivMjesta, Datum, OpisPOsla, UtrosenoVrijeme, ID_Mjesta) VALUES ('Rijek2','2019-01-10 11:15:12','Opis posla je jako bitan da bi firma radila pravilno i da se vidi potrosnja',12,1);
-INSERT INTO `Evidencija` (NazivMjesta, Datum, OpisPOsla, UtrosenoVrijeme, ID_Mjesta) VALUES ('Crkvenica','2019-01-10 11:16:43','opis posla kazes a',4,1);
-INSERT INTO `Evidencija` (NazivMjesta, Datum, OpisPOsla, UtrosenoVrijeme, ID_Mjesta) VALUES ('Pula','2019-01-10 12:05:47','RADI VALJDA HAHAHAHAHAHAHAHAHAHAHAHAHAHA',10,1);
+INSERT INTO `Evidencija` (NazivMjesta, Datum, OpisPOsla, UtrosenoVrijeme, ID_Mjesta) VALUES ('Rijek2','2019-01-15 11:15:12','Opis posla je jako bitan da bi firma radila pravilno i da se vidi potrosnja',12,1);
+INSERT INTO `Evidencija` (NazivMjesta, Datum, OpisPOsla, UtrosenoVrijeme, ID_Mjesta) VALUES ('Crkvenica','2019-01-20 11:16:43','opis posla kazes a',4,1);
+INSERT INTO `Evidencija` (NazivMjesta, Datum, OpisPOsla, UtrosenoVrijeme, ID_Mjesta) VALUES ('Pula','2019-01-22 12:05:47','RADI VALJDA HAHAHAHAHAHAHAHAHAHAHAHAHAHA',10,1);
 
-insert into `Poveznica` (Id_Evidencija,Id_Materijala,Kolicina) VALUES (1,1, 220);
-insert into `Poveznica` (Id_Evidencija,Id_Materijala,Kolicina) VALUES (1,2, 50);
-insert into `Poveznica` (Id_Evidencija,Id_Materijala,Kolicina) VALUES (1,3, 1);
-insert into `Poveznica` (Id_Evidencija,Id_Materijala,Kolicina) VALUES (1,4, 2);
-insert into `Poveznica` (Id_Evidencija,Id_Materijala,Kolicina) VALUES (1,5, 10);
-insert into `Poveznica` (Id_Evidencija,Id_Materijala,Kolicina) VALUES (1,6, 5);
-insert into `Poveznica` (Id_Evidencija,Id_Materijala,Kolicina) VALUES (1,7, 10);
-insert into `Poveznica` (Id_Evidencija,Id_Materijala,Kolicina) VALUES (1,8, 15);
-insert into `Poveznica` (Id_Evidencija,Id_Materijala,Kolicina) VALUES (1,9, 5);
-insert into `Poveznica` (Id_Evidencija,Id_Materijala,Kolicina) VALUES (1,10, 13);
+insert into `Poveznica` (Id_Evidencija,Id_Materijala,Kolicina,NazivMaterijala) VALUES (1,1, 22,'Drva');
+insert into `Poveznica` (Id_Evidencija,Id_Materijala,Kolicina,NazivMaterijala) VALUES (1,2, 50,'Cigle');
+insert into `Poveznica` (Id_Evidencija,Id_Materijala,Kolicina,NazivMaterijala) VALUES (1,3, 1,'PISTOLJ');
+insert into `Poveznica` (Id_Evidencija,Id_Materijala,Kolicina,NazivMaterijala) VALUES (1,4, 2,'Šmajser');
+insert into `Poveznica` (Id_Evidencija,Id_Materijala,Kolicina,NazivMaterijala) VALUES (1,5, 10,'WD 40');
+insert into `Poveznica` (Id_Evidencija,Id_Materijala,Kolicina,NazivMaterijala) VALUES (1,6, 5,'Hard Disk 10TB');
+insert into `Poveznica` (Id_Evidencija,Id_Materijala,Kolicina,NazivMaterijala) VALUES (1,7, 10,'SSD 250GB ');
+insert into `Poveznica` (Id_Evidencija,Id_Materijala,Kolicina,NazivMaterijala) VALUES (1,8, 15,'SSD 500GB ');
+insert into `Poveznica` (Id_Evidencija,Id_Materijala,Kolicina,NazivMaterijala) VALUES (1,9, 5,'Hard Disk 5TB');
+insert into `Poveznica` (Id_Evidencija,Id_Materijala,Kolicina,NazivMaterijala) VALUES (1,10, 13,'Ram 8GB');
 
-INSERT INTO `Poveznica` (NazivMaterijala, Id_Materijala,Kolicina, Cijena, Id_Evidencija) VALUES ('Drva',7,5,10.0,3);
+
+INSERT INTO `Poveznica` (NazivMaterijala, Id_Materijala,Kolicina, Cijena, Id_Evidencija) VALUES ('Drva',1,5,10.0,2);
+INSERT INTO `Poveznica` (NazivMaterijala, Id_Materijala,Kolicina, Cijena, Id_Evidencija) VALUES ('Drva',1,5,10.0,3);
+INSERT INTO `Poveznica` (NazivMaterijala, Id_Materijala,Kolicina, Cijena, Id_Evidencija) VALUES ('Drva',1,-5,10.0,4);
+INSERT INTO `Poveznica` (NazivMaterijala, Id_Materijala,Kolicina, Cijena, Id_Evidencija) VALUES ('Drva',1,-15,10.0,5);
