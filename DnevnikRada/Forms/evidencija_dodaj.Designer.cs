89d790767a4c792cbd32096107e5a550b9270f3c
sandiivanusec <s.ivanusec@gmail.com>
2019-01-22 14:29:10 +0100

    izmijenjen dizajn cijele evidencije dodaj

--- DnevnikRada/Forms/evidencija_dodaj.Designer.cs
+++ DnevnikRada/Forms/evidencija_dodaj.Designer.cs
@@ -71,7 +71,7 @@
             // 
             this.lbl_naziv_mjesta.AutoSize = true;
             this.lbl_naziv_mjesta.FontSize = MetroFramework.MetroLabelSize.Tall;
-            this.lbl_naziv_mjesta.Location = new System.Drawing.Point(12, 90);
+            this.lbl_naziv_mjesta.Location = new System.Drawing.Point(72, 36);
             this.lbl_naziv_mjesta.Name = "lbl_naziv_mjesta";
             this.lbl_naziv_mjesta.Size = new System.Drawing.Size(108, 25);
             this.lbl_naziv_mjesta.TabIndex = 1;
@@ -81,7 +81,7 @@
             // 
             this.lbl_utroseno_vrijeme.AutoSize = true;
             this.lbl_utroseno_vrijeme.FontSize = MetroFramework.MetroLabelSize.Tall;
-            this.lbl_utroseno_vrijeme.Location = new System.Drawing.Point(12, 182);
+            this.lbl_utroseno_vrijeme.Location = new System.Drawing.Point(72, 128);
             this.lbl_utroseno_vrijeme.Name = "lbl_utroseno_vrijeme";
             this.lbl_utroseno_vrijeme.Size = new System.Drawing.Size(143, 25);
             this.lbl_utroseno_vrijeme.TabIndex = 2;
@@ -93,16 +93,16 @@
             // 
             // 
             this.tb_utroseno_vrijeme.CustomButton.Image = null;
-            this.tb_utroseno_vrijeme.CustomButton.Location = new System.Drawing.Point(178, 1);
+            this.tb_utroseno_vrijeme.CustomButton.Location = new System.Drawing.Point(172, 1);
             this.tb_utroseno_vrijeme.CustomButton.Name = "";
-            this.tb_utroseno_vrijeme.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.tb_utroseno_vrijeme.CustomButton.Size = new System.Drawing.Size(27, 27);
             this.tb_utroseno_vrijeme.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
             this.tb_utroseno_vrijeme.CustomButton.TabIndex = 1;
             this.tb_utroseno_vrijeme.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
             this.tb_utroseno_vrijeme.CustomButton.UseSelectable = true;
             this.tb_utroseno_vrijeme.CustomButton.Visible = false;
             this.tb_utroseno_vrijeme.Lines = new string[0];
-            this.tb_utroseno_vrijeme.Location = new System.Drawing.Point(168, 184);
+            this.tb_utroseno_vrijeme.Location = new System.Drawing.Point(223, 124);
             this.tb_utroseno_vrijeme.MaxLength = 32767;
             this.tb_utroseno_vrijeme.Name = "tb_utroseno_vrijeme";
             this.tb_utroseno_vrijeme.PasswordChar = '\0';
@@ -111,7 +111,7 @@
             this.tb_utroseno_vrijeme.SelectionLength = 0;
             this.tb_utroseno_vrijeme.SelectionStart = 0;
             this.tb_utroseno_vrijeme.ShortcutsEnabled = true;
-            this.tb_utroseno_vrijeme.Size = new System.Drawing.Size(200, 23);
+            this.tb_utroseno_vrijeme.Size = new System.Drawing.Size(200, 29);
             this.tb_utroseno_vrijeme.TabIndex = 4;
             this.tb_utroseno_vrijeme.UseSelectable = true;
             this.tb_utroseno_vrijeme.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
@@ -120,17 +120,17 @@
             // 
             // tb_opis_posla
             // 
-            this.tb_opis_posla.Location = new System.Drawing.Point(12, 235);
+            this.tb_opis_posla.Location = new System.Drawing.Point(0, 211);
             this.tb_opis_posla.Name = "tb_opis_posla";
-            this.tb_opis_posla.Size = new System.Drawing.Size(356, 333);
+            this.tb_opis_posla.Size = new System.Drawing.Size(495, 362);
             this.tb_opis_posla.TabIndex = 5;
-            this.tb_opis_posla.Text = "Opis Posla";
+            this.tb_opis_posla.Text = "";
             // 
             // lbl_opis_posla
             // 
             this.lbl_opis_posla.AutoSize = true;
             this.lbl_opis_posla.FontSize = MetroFramework.MetroLabelSize.Tall;
-            this.lbl_opis_posla.Location = new System.Drawing.Point(140, 207);
+            this.lbl_opis_posla.Location = new System.Drawing.Point(205, 183);
             this.lbl_opis_posla.Name = "lbl_opis_posla";
             this.lbl_opis_posla.Size = new System.Drawing.Size(91, 25);
             this.lbl_opis_posla.TabIndex = 6;
@@ -142,7 +142,7 @@
             this.Potvrdi.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Potvrdi.BackgroundImage")));
             this.Potvrdi.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.Potvrdi.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.Potvrdi.Location = new System.Drawing.Point(701, 480);
+            this.Potvrdi.Location = new System.Drawing.Point(700, 480);
             this.Potvrdi.Name = "Potvrdi";
             this.Potvrdi.Size = new System.Drawing.Size(100, 100);
             this.Potvrdi.TabIndex = 7;
@@ -155,7 +155,7 @@
             this.btn_trash.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("btn_trash.BackgroundImage")));
             this.btn_trash.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.btn_trash.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.btn_trash.Location = new System.Drawing.Point(602, 480);
+            this.btn_trash.Location = new System.Drawing.Point(600, 480);
             this.btn_trash.Name = "btn_trash";
             this.btn_trash.Size = new System.Drawing.Size(100, 100);
             this.btn_trash.TabIndex = 8;
@@ -167,7 +167,7 @@
             this.Dodaj.BackgroundImage = ((System.Drawing.Image)(resources.GetObject("Dodaj.BackgroundImage")));
             this.Dodaj.BackgroundImageLayout = System.Windows.Forms.ImageLayout.Center;
             this.Dodaj.Cursor = System.Windows.Forms.Cursors.Hand;
-            this.Dodaj.Location = new System.Drawing.Point(503, 480);
+            this.Dodaj.Location = new System.Drawing.Point(500, 480);
             this.Dodaj.Name = "Dodaj";
             this.Dodaj.Size = new System.Drawing.Size(100, 100);
             this.Dodaj.TabIndex = 9;
@@ -180,16 +180,16 @@
             // 
             // 
             this.Kolicina.CustomButton.Image = null;
-            this.Kolicina.CustomButton.Location = new System.Drawing.Point(37, 1);
+            this.Kolicina.CustomButton.Location = new System.Drawing.Point(32, 1);
             this.Kolicina.CustomButton.Name = "";
-            this.Kolicina.CustomButton.Size = new System.Drawing.Size(21, 21);
+            this.Kolicina.CustomButton.Size = new System.Drawing.Size(27, 27);
             this.Kolicina.CustomButton.Style = MetroFramework.MetroColorStyle.Blue;
             this.Kolicina.CustomButton.TabIndex = 1;
             this.Kolicina.CustomButton.Theme = MetroFramework.MetroThemeStyle.Light;
             this.Kolicina.CustomButton.UseSelectable = true;
             this.Kolicina.CustomButton.Visible = false;
             this.Kolicina.Lines = new string[0];
-            this.Kolicina.Location = new System.Drawing.Point(819, 92);
+            this.Kolicina.Location = new System.Drawing.Point(795, 46);
             this.Kolicina.MaxLength = 32767;
             this.Kolicina.Name = "Kolicina";
             this.Kolicina.PasswordChar = '\0';
@@ -198,7 +198,7 @@
             this.Kolicina.SelectionLength = 0;
             this.Kolicina.SelectionStart = 0;
             this.Kolicina.ShortcutsEnabled = true;
-            this.Kolicina.Size = new System.Drawing.Size(59, 23);
+            this.Kolicina.Size = new System.Drawing.Size(60, 29);
             this.Kolicina.TabIndex = 11;
             this.Kolicina.UseSelectable = true;
             this.Kolicina.WaterMarkColor = System.Drawing.Color.FromArgb(((int)(((byte)(109)))), ((int)(((byte)(109)))), ((int)(((byte)(109)))));
@@ -209,17 +209,17 @@
             // 
             this.metroLabel1.AutoSize = true;
             this.metroLabel1.FontSize = MetroFramework.MetroLabelSize.Tall;
-            this.metroLabel1.Location = new System.Drawing.Point(426, 90);
+            this.metroLabel1.Location = new System.Drawing.Point(549, 12);
             this.metroLabel1.Name = "metroLabel1";
-            this.metroLabel1.Size = new System.Drawing.Size(152, 25);
+            this.metroLabel1.Size = new System.Drawing.Size(145, 25);
             this.metroLabel1.TabIndex = 10;
-            this.metroLabel1.Text = "Utrosen Materijal 1";
+            this.metroLabel1.Text = "Utrošeni Materijal";
             // 
             // OdabirMaterijala
             // 
             this.OdabirMaterijala.FormattingEnabled = true;
             this.OdabirMaterijala.ItemHeight = 23;
-            this.OdabirMaterijala.Location = new System.Drawing.Point(587, 92);
+            this.OdabirMaterijala.Location = new System.Drawing.Point(537, 46);
             this.OdabirMaterijala.Name = "OdabirMaterijala";
             this.OdabirMaterijala.Size = new System.Drawing.Size(168, 29);
             this.OdabirMaterijala.TabIndex = 18;
@@ -229,17 +229,17 @@
             // 
             this.metroLabel5.AutoSize = true;
             this.metroLabel5.FontSize = MetroFramework.MetroLabelSize.Tall;
-            this.metroLabel5.Location = new System.Drawing.Point(754, 50);
+            this.metroLabel5.Location = new System.Drawing.Point(726, 13);
             this.metroLabel5.Name = "metroLabel5";
-            this.metroLabel5.Size = new System.Drawing.Size(134, 25);
+            this.metroLabel5.Size = new System.Drawing.Size(143, 25);
             this.metroLabel5.TabIndex = 22;
-            this.metroLabel5.Text = "Utrosen Kolicina";
+            this.metroLabel5.Text = "Utrošena Količina";
             // 
             // addUse
             // 
             this.addUse.FormattingEnabled = true;
             this.addUse.ItemHeight = 23;
-            this.addUse.Location = new System.Drawing.Point(767, 92);
+            this.addUse.Location = new System.Drawing.Point(740, 46);
             this.addUse.Name = "addUse";
             this.addUse.Size = new System.Drawing.Size(46, 29);
             this.addUse.TabIndex = 23;
@@ -258,7 +258,7 @@
             // 
             this.metroComboBox9.FormattingEnabled = true;
             this.metroComboBox9.ItemHeight = 23;
-            this.metroComboBox9.Location = new System.Drawing.Point(168, 92);
+            this.metroComboBox9.Location = new System.Drawing.Point(223, 32);
             this.metroComboBox9.Name = "metroComboBox9";
             this.metroComboBox9.Size = new System.Drawing.Size(200, 29);
             this.metroComboBox9.TabIndex = 30;
@@ -293,7 +293,7 @@
             this.materijalGrid.EnableHeadersVisualStyles = false;
             this.materijalGrid.Font = new System.Drawing.Font("Segoe UI", 11F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Pixel);
             this.materijalGrid.GridColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(255)))), ((int)(((byte)(255)))));
-            this.materijalGrid.Location = new System.Drawing.Point(426, 137);
+            this.materijalGrid.Location = new System.Drawing.Point(500, 94);
             this.materijalGrid.Name = "materijalGrid";
             this.materijalGrid.ReadOnly = true;
             this.materijalGrid.RowHeadersBorderStyle = System.Windows.Forms.DataGridViewHeaderBorderStyle.None;
@@ -307,7 +307,7 @@
             this.materijalGrid.RowHeadersDefaultCellStyle = dataGridViewCellStyle3;
             this.materijalGrid.RowHeadersWidthSizeMode = System.Windows.Forms.DataGridViewRowHeadersWidthSizeMode.DisableResizing;
             this.materijalGrid.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect;
-            this.materijalGrid.Size = new System.Drawing.Size(462, 325);
+            this.materijalGrid.Size = new System.Drawing.Size(400, 380);
             this.materijalGrid.TabIndex = 31;
             this.materijalGrid.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.evidencijaGrid_CellClick);
             // 
@@ -315,7 +315,7 @@
             // 
             this.metroLabel2.AutoSize = true;
             this.metroLabel2.FontSize = MetroFramework.MetroLabelSize.Tall;
-            this.metroLabel2.Location = new System.Drawing.Point(12, 137);
+            this.metroLabel2.Location = new System.Drawing.Point(72, 81);
             this.metroLabel2.Name = "metroLabel2";
             this.metroLabel2.Size = new System.Drawing.Size(63, 25);
             this.metroLabel2.TabIndex = 32;
@@ -323,7 +323,7 @@
             // 
             // metroDateTime1
             // 
-            this.metroDateTime1.Location = new System.Drawing.Point(168, 137);
+            this.metroDateTime1.Location = new System.Drawing.Point(223, 77);
             this.metroDateTime1.MinimumSize = new System.Drawing.Size(0, 29);
             this.metroDateTime1.Name = "metroDateTime1";
             this.metroDateTime1.Size = new System.Drawing.Size(200, 29);
