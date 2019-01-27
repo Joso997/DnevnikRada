b5ec5deaa99653890a382ae680801f0e8259e5df
Danko <danko.salopek1@gmail.com>
2019-01-27 12:20:53 +0100

    Brisanje
    brisanje nepotrebnogg iz statistike

--- DnevnikRada/Kontrole/BrojOdrzavanjaKrozVrijeme.cs
+++ DnevnikRada/Kontrole/BrojOdrzavanjaKrozVrijeme.cs
@@ -25,14 +25,11 @@ namespace DnevnikRada
             int i = 0;
             string SveEvidencije = "select DISTINCT  date(Datum) from Evidencija";
             SQLiteDataReader data = baza.GraphReaders(SveEvidencije);
-            //Chart.Series[0].BorderWidth = 5;
             Chart.Series[0].ChartType = System.Windows.Forms.DataVisualization.Charting.SeriesChartType.Column;
             while (data.Read())
             {
                 DateTime date = data.GetDateTime(0);
                 string brojZaOdređeniDatum = string.Format("select count (ID) from Evidencija where date(datum)=" + '"' + "{0}" + '"',data.GetString(0));
-                
-                //MessageBox.Show(Convert.ToString(brojZaOdređeniDatum+"    "+ i));
                 SQLiteDataReader count = baza.GraphReaders(brojZaOdređeniDatum);
                 while (count.Read())
                 {
