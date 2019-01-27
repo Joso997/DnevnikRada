de1fb1e91bb1681f0ffe987a35b5461debdcb5f9
sandiivanusec <s.ivanusec@gmail.com>
2019-01-27 11:03:31 +0100

    izbrisane stvari s odstupanje od normale

--- DnevnikRada/Kontrole/OdstupanjeOdNormale.Designer.cs
+++ DnevnikRada/Kontrole/OdstupanjeOdNormale.Designer.cs
@@ -28,82 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
-            this.metroDateTime1 = new MetroFramework.Controls.MetroDateTime();
-            this.metroDateTime2 = new MetroFramework.Controls.MetroDateTime();
-            this.metroDateTime3 = new MetroFramework.Controls.MetroDateTime();
-            this.metroDateTime4 = new MetroFramework.Controls.MetroDateTime();
-            this.metroDateTime5 = new MetroFramework.Controls.MetroDateTime();
-            this.metroDateTime6 = new MetroFramework.Controls.MetroDateTime();
-            this.metroDateTime7 = new MetroFramework.Controls.MetroDateTime();
             this.SuspendLayout();
-            // 
-            // metroDateTime1
-            // 
-            this.metroDateTime1.Location = new System.Drawing.Point(343, 150);
-            this.metroDateTime1.MinimumSize = new System.Drawing.Size(0, 29);
-            this.metroDateTime1.Name = "metroDateTime1";
-            this.metroDateTime1.Size = new System.Drawing.Size(200, 29);
-            this.metroDateTime1.TabIndex = 0;
-            // 
-            // metroDateTime2
-            // 
-            this.metroDateTime2.Location = new System.Drawing.Point(182, 104);
-            this.metroDateTime2.MinimumSize = new System.Drawing.Size(0, 29);
-            this.metroDateTime2.Name = "metroDateTime2";
-            this.metroDateTime2.Size = new System.Drawing.Size(200, 29);
-            this.metroDateTime2.TabIndex = 0;
-            // 
-            // metroDateTime3
-            // 
-            this.metroDateTime3.Location = new System.Drawing.Point(174, 150);
-            this.metroDateTime3.MinimumSize = new System.Drawing.Size(0, 29);
-            this.metroDateTime3.Name = "metroDateTime3";
-            this.metroDateTime3.Size = new System.Drawing.Size(200, 29);
-            this.metroDateTime3.TabIndex = 0;
-            // 
-            // metroDateTime4
-            // 
-            this.metroDateTime4.Location = new System.Drawing.Point(13, 104);
-            this.metroDateTime4.MinimumSize = new System.Drawing.Size(0, 29);
-            this.metroDateTime4.Name = "metroDateTime4";
-            this.metroDateTime4.Size = new System.Drawing.Size(200, 29);
-            this.metroDateTime4.TabIndex = 0;
-            // 
-            // metroDateTime5
-            // 
-            this.metroDateTime5.Location = new System.Drawing.Point(215, 69);
-            this.metroDateTime5.MinimumSize = new System.Drawing.Size(0, 29);
-            this.metroDateTime5.Name = "metroDateTime5";
-            this.metroDateTime5.Size = new System.Drawing.Size(200, 29);
-            this.metroDateTime5.TabIndex = 0;
-            // 
-            // metroDateTime6
-            // 
-            this.metroDateTime6.Location = new System.Drawing.Point(207, 115);
-            this.metroDateTime6.MinimumSize = new System.Drawing.Size(0, 29);
-            this.metroDateTime6.Name = "metroDateTime6";
-            this.metroDateTime6.Size = new System.Drawing.Size(200, 29);
-            this.metroDateTime6.TabIndex = 0;
-            // 
-            // metroDateTime7
-            // 
-            this.metroDateTime7.Location = new System.Drawing.Point(46, 69);
-            this.metroDateTime7.MinimumSize = new System.Drawing.Size(0, 29);
-            this.metroDateTime7.Name = "metroDateTime7";
-            this.metroDateTime7.Size = new System.Drawing.Size(200, 29);
-            this.metroDateTime7.TabIndex = 0;
             // 
             // ucModule4
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.Controls.Add(this.metroDateTime7);
-            this.Controls.Add(this.metroDateTime6);
-            this.Controls.Add(this.metroDateTime4);
-            this.Controls.Add(this.metroDateTime5);
-            this.Controls.Add(this.metroDateTime3);
-            this.Controls.Add(this.metroDateTime2);
-            this.Controls.Add(this.metroDateTime1);
             this.Name = "ucModule4";
             this.Size = new System.Drawing.Size(620, 578);
             this.ResumeLayout(false);
@@ -111,13 +41,5 @@
         }
 
         #endregion
-
-        private MetroFramework.Controls.MetroDateTime metroDateTime1;
-        private MetroFramework.Controls.MetroDateTime metroDateTime2;
-        private MetroFramework.Controls.MetroDateTime metroDateTime3;
-        private MetroFramework.Controls.MetroDateTime metroDateTime4;
-        private MetroFramework.Controls.MetroDateTime metroDateTime5;
-        private MetroFramework.Controls.MetroDateTime metroDateTime6;
-        private MetroFramework.Controls.MetroDateTime metroDateTime7;
     }
 }
